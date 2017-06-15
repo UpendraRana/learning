417a0435e62587cacd569d0a9495f1a2eafb1c80
Upendra <upendra.rana@hotmail.com>
2017-06-15 16:21:39 -0400

    Added configuration service, Custom directive for responsive desing

--- green-nations/src/fw/fw.module.ts
+++ green-nations/src/fw/fw.module.ts
@@ -3,15 +3,33 @@ import { CommonModule } from '@angular/common';
 import { FrameworkBodyComponent } from "fw/framework-body/framework-body.component";
 import { ContentComponent } from "fw/content/content.component";
 import { TitleBarComponent } from "fw/title-bar/title-bar.component";
+import { FrameworkConfigService } from "fw/service/framework-config.service";
+import { TopBarComponent } from "fw/top-bar/top-bar.component";
+import { StatusBarComponent } from "fw/status-bar/status-bar.component";
+import { ScreenService } from "fw/service/screen.service";
+import { ScreenLarge } from "fw/directives/screen-large.directive";
+import { ScreenBelowLarge } from "fw/directives/screen-below-large.directive";
+
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
+  providers: [
+    FrameworkConfigService,
+    ScreenService
+  ],
   declarations: [
     FrameworkBodyComponent,
     ContentComponent,
-    TitleBarComponent],
+    TitleBarComponent,
+    TopBarComponent,
+    StatusBarComponent,
+    ScreenLarge,
+    ScreenBelowLarge
+  ],
+
   exports: [FrameworkBodyComponent]
 })
 export class FwModule { }
